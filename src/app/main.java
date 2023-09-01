a3635f732e802257a0a94d7d7a2f0ee2fdc04132
AndreyK1993 <andrey.kurilenko27@gmail.com>
2023-09-01 22:49:33 +0300

    Operators-Logical

--- src/app/main.java
+++ src/app/main.java
@@ -9,27 +9,20 @@ public class main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Input int value from 0 to 10000 a: ");
+        System.out.println("Input int value a: ");
         int a = sc.nextInt();
 
-        System.out.println("Input int value from 10001 to 25000 b: ");
-        int b = sc.nextInt();
 
-        System.out.println("Input int value from 25001 and bigger c: ");
-        int c = sc.nextInt();
-
-        if (0 < a | a < 10000) {
+        if (0 < a || a < 10001) {
             double res = a * .025;
             System.out.printf("Result is %.2f", res);
 
-        } else if (10001 < b | b < 25000) {
-
-            double res = b * .043;
-
+        }  if (a > 10001 || a <= 25001) {
+            double res = a * .043;
             System.out.printf("Result is %.2f", res);
 
-        } else if (c > 25000) {
-            double res = c * .067;
+        }  if (a > 25001) {
+            double res = a * .067;
             System.out.printf("Result is %.2f", res);
 
             sc.close();
